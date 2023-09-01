2d46944244446fc9781e7977d312f329ce3d9548
Yonko-Kunal <128897093+Yonko-Kunal@users.noreply.github.com>
2023-09-01 15:53:40 +0530

    made it
    I made my first rocket

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // console.log('hello my friend');
+var timer = null;
+var countDownNumber = 10;
+
+var changeState = function (state) {
+    document.body.className = 'body-state' + state;
+    clearInterval(timer);
+    countDownNumber = 10;
+    document.getElementById('countDown').innerHTML = countDownNumber;
+
+    if (state == 2) {
+        timer = setInterval(function () {
+            countDownNumber = countDownNumber - 1;
+            document.getElementById('countDown').innerHTML = countDownNumber;
+
+            if (countDownNumber <= -1) {
+                changeState(3);
+            }
+        }, 300);
+    } else if (state == 3) {
+        var success = setTimeout(function () {
+            var randomNumber = Math.round(Math.random() * 10);
+            console.log('randomNumber : ', randomNumber);
+            //succes condition
+            if (randomNumber >= 5) {
+                changeState(4); // success
+            } else {
+                changeState(5); // faliure
+            };
+        }, 2000)
+    };
+}
